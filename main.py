475390cc70f5e82d9826d44a8b2507540cf1b755
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-27 22:29:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
-import pyrogram
-from pyrogram import filters
-from pymongo import MongoClient
+import logging
+import telebot
 
-# Initialize the Pyrogram client
-app = pyrogram.Client(
-    "my_bot",
-    api_id="18654447",
-    api_hash="60ac6f65c766e73dfcc1debef93d06bc",
-    bot_token="5058249365:AAHVL7ozsLYA6N3dHtUTAVkZ3sv_YniZdOk"
-)
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Connect to MongoDB
-mongo_client = MongoClient("mongodb+srv://abc:abcd@cluster0.0g1g1gt.mongodb.net/?retryWrites=true&w=majority")
-db = mongo_client["chatbot_db"]
+# Telegram Bot Token
+TOKEN = '6101196560:AAE9Te6XfIfldcJcqdnh6Yb7SHPVi_z3hRc'
 
-# Handler for incoming messages
-@app.on_message(filters.text)
-def handle_message(client, message):
-    # Get the chat ID
+# Create bot instance
+bot = telebot.TeleBot(TOKEN)
+
+# Dictionary to store the last message for each chat
+last_messages = {}
+
+# Handler for generating auto-responses
+@bot.message_handler(func=lambda message: True)
+def auto_response(message):
     chat_id = message.chat.id
 
-    # Get or create the chat document in the database
-    chat = db.chats.find_one({"chat_id": chat_id})
-    if not chat:
-        chat = {"chat_id": chat_id, "data": message.text}
-        db.chats.insert_one(chat)
-        reply_text = "Hello! How can I assist you today?"
-    else:
-        # Retrieve the data associated with the chat
-        data = chat["data"]
+    # Retrieve the last message in the chat
+    last_message = last_messages.get(chat_id)
+    if last_message:
+        # Generate auto-response based on the last message
+        auto_reply = f"Thank you for your message: {last_message.text}"
+        bot.reply_to(message, auto_reply)
 
-        # Process the incoming message and generate a reply
-        # Replace this with your AI model or any other logic
-        reply_text = f"You said: {message.text}. Your data: {data}"
+    # Store the current message as the last message for future reference
+    last_messages[chat_id] = message
 
-        # Update the data in the database
-        db.chats.update_one({"chat_id": chat_id}, {"$set": {"data": message.text}})
-
-    # Send the reply
-    client.send_message(chat_id, reply_text)
-
-# Start the bot
-app.run()
+# Start the Bot
+bot.polling()
